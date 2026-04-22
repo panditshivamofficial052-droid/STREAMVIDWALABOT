ed0e3008e9bf5f3882da359b4be589fdcbf0490c
sheffykhlg <56907336+sheffykhlg@users.noreply.github.com>
2026-04-22 15:17:55 +0530

    Update config.py

--- config.py
+++ config.py
@@ -4,11 +4,11 @@ from dotenv import load_dotenv
 load_dotenv()
 
 class Config:
-    API_ID = int(os.environ.get("API_ID", "22135296"))
-    API_HASH = os.environ.get("API_HASH", "b3051c4c2dfe4ef65f7146d172d3ddaf")
+    API_ID = int(os.environ.get("API_ID", "39193426"))
+    API_HASH = os.environ.get("API_HASH", "dec352035203bc35933ca952e19e5b1d")
     BOT_TOKEN = os.environ.get("BOT_TOKEN", "8552560424:AAFJpVqHLb_AyYYkK6WOhrywCenqJjGrP3E")
     
-    _owner_raw = os.environ.get("OWNER_ID", "7893435873 7010804219").replace(",", " ")
+    _owner_raw = os.environ.get("OWNER_ID", "7010804219").replace(",", " ")
     OWNER_ID = [int(x) for x in _owner_raw.split() if x.strip().isdigit()]
     
     MONGO_URI = os.environ.get("MONGO_URI", "mongodb+srv://a:a@loude.0uy8em5.mongodb.net/?appName=LOUDE")
